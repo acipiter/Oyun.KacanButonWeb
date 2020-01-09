c7318043e68605fdbeb62ffda4dedf37f14dd11e
Sahin Gundogdu <shngndgd@hotmail.com>
2020-01-09 15:09:18 +0300

    Add project files.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Oyun.KacanButonWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Oyun.KacanButonWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("686d97d0-5058-42cb-a518-e8841f05e43a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ js/script.js
@@ -0,0 +1,27 @@
+﻿var text = document.getElementById("text");
+var kare = document.getElementById("kare");
+var kareW = kare.offsetWidth;
+var kareH = kare.offsetHeight;
+
+
+function ortala() {
+    var w = window.innerWidth; //tarayıcının iç genişliğini aldım
+    var h = window.innerHeight; //tarayıcının iç yüksekliğini aldım
+    var ilkL = kare.style.left = w / 2 - kareW / 2 + "px";
+    var ilkT = kare.style.top = h / 2 - kareH / 2 + "px";
+    text.innerHTML = "T: " + ilkT + "<br />L: " + ilkL;
+}
+
+ortala();
+
+function kac() {
+    var w = window.innerWidth; //tarayıcının iç genişliğini aldım
+    var h = window.innerHeight; //tarayıcının iç yüksekliğini aldım
+    var rndT = Math.floor(Math.random() * (h - kareH)); //Top değeri için rastgele bi sayı ürettim. Viewport yüksekliğinden (h) karenin yüksekliğini çıkarttım ki alta taşmayı engellemek için
+    var rndL = Math.floor(Math.random() * (w - kareW));
+
+    kare.style.left = rndL + "px";
+    kare.style.top = rndT + "px";
+    text.innerHTML = "T: " + rndT + "px<br />L: " + rndL+"px";
+}
+
